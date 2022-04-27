e43f4356d1865333afce6c988f95d7ecc475d9c4
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-28 02:28:17 +0600

    reset on hover. Final commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const sections = document.querySelectorAll(".section")
-const button = document.querySelector("button")
+const buttons = document.querySelectorAll(".button")
+const section1 = document.querySelector(".section-1")
+const section2 = document.querySelector(".section-2")
+const back = document.querySelector(".back")
 
 sections.forEach(section => {
 
@@ -34,3 +37,25 @@ sections.forEach(section => {
     })
 })
 
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        const btnClass = e.target.classList;
+
+        if (btnClass.contains('ios')) {
+            section1.style.minWidth = '100vw'
+            section2.style.minWidth = '0vw'
+            section2.style.overflow = 'hidden'
+        }
+
+        if (btnClass.contains('android')) {
+            section2.style.minWidth = '100vw'
+            section1.style.minWidth = '0vw'
+            section2.style.overflow = 'hidden'
+        }
+    })
+})
+
+back.addEventListener("click", e => {
+    section1.style.minWidth = '20vw'
+    section2.style.minWidth = '20vw'
+})
